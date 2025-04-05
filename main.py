decb49868e2dda8182afaf9819deb23abc250217
Ankit Prajapati <ap8424856@gmail.com>
2025-04-05 16:56:07 +0530

    updated main with config and env

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import yaml
-
+from os import environ
 from scrapper.app import ScrapperApp
 
 with open("config.yaml", "r") as cfile:
     config = yaml.safe_load(cfile)
+    config["db_url"] = environ["DATABASE_URL"]
+    config["workers"] = int(environ["WORKERS"])
+    config["proxies"] = {
+        "http": environ["PROXIES"],
+        "https": environ["PROXIES"]
+    }
 
 if __name__ == "__main__":
     app = ScrapperApp(config)
     app.scrap_links()
-    app.scrap_data(workers=int(config["workers"]))
+    app.scrap_data(workers=config["workers"])
     app.upload()

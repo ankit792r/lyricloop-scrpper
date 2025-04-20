87f2053e40c5d7e14e40f546070aa17faf3c3019
Ankit Prajapati <ap8424856@gmail.com>
2025-04-20 18:24:28 +0530

    created bulk scrapper

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from string import ascii_lowercase
 
-page = requests.get(f"https://www.ilyricshub.com/hindi/page/2")
-                
-page_soup = BeautifulSoup(page.text, "html.parser")
-
-link_tags = page_soup.find_all("a", {"class": "block bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-200"})
-
-
-for a in link_tags:
-    print(a.attrs["href"])
+for alpha in ascii_lowercase:
+    try:
+        page = requests.get(f"https://www.hindilyrics4u.com/movie/{alpha}.php")
+        page.raise_for_status()
+        page_soup_temp = BeautifulSoup(page.text, "html.parser")
+        text:str = page_soup_temp.find("td", {"class": "alcen w720 bg7f"}).text
+        print(text.split(" ").pop())
+    except:
+        print("failed for ", alpha)
